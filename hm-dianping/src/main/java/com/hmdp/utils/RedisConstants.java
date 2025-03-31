3c1c36e0d3f5ab20d5eab6e80e9d32fca1349449
xiaozayula <981446636@qq.com>
2025-03-31 17:36:06 +0800

    添加字段

--- hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
@@ -10,9 +10,10 @@ public class RedisConstants {
 
     public static final Long CACHE_SHOP_TTL = 30L;
     public static final String CACHE_SHOP_KEY = "cache:shop:";
-
+    public static final String CACHE_SHOP_TYPE_KEY="cache:shoptype:";
     public static final String LOCK_SHOP_KEY = "lock:shop:";
     public static final Long LOCK_SHOP_TTL = 10L;
+    public static final Long CACHE_SHOP_TYPE_TTL = 10L;
 
     public static final String SECKILL_STOCK_KEY = "seckill:stock:";
     public static final String BLOG_LIKED_KEY = "blog:liked:";
