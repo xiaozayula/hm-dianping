278941118ab2c62f194b3ace3dd0e8fef12cbc4c
xiaozayula <981446636@qq.com>
2025-04-01 15:39:28 +0800

    redis实现全局唯一id，时间戳+序列号

--- hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisConstants.java
@@ -14,10 +14,13 @@ public class RedisConstants {
     public static final String LOCK_SHOP_KEY = "lock:shop:";
     public static final Long LOCK_SHOP_TTL = 10L;
     public static final Long CACHE_SHOP_TYPE_TTL = 10L;
+    public static final Long CACHE_SHOP_LOGICAL_TTL = 10L;
 
     public static final String SECKILL_STOCK_KEY = "seckill:stock:";
     public static final String BLOG_LIKED_KEY = "blog:liked:";
     public static final String FEED_KEY = "feed:";
+    public static final String ID_PREFIX  = "icr:";
+
     public static final String SHOP_GEO_KEY = "shop:geo:";
     public static final String USER_SIGN_KEY = "sign:";
 }
