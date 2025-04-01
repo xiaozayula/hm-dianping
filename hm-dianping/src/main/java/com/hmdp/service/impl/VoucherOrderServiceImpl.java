ae4288fb0237d581c2eb3180f64d2f798feef196
xiaozayula <981446636@qq.com>
2025-04-01 17:21:49 +0800

    实现秒杀下单

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,20 +1,70 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-/**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+    @Resource
+    private RedisIdWorker redisIdWorker;
+    /**
+     * 抢购秒杀券
+     *
+     * @param voucherId
+     * @return
+     */
+    @Transactional
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        // 1、查询秒杀券
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        // 2、判断秒杀券是否合法
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            // 秒杀券的开始时间在当前时间之后
+            return Result.fail("秒杀尚未开始");
+        }
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            // 秒杀券的结束时间在当前时间之前
+            return Result.fail("秒杀已结束");
+        }
+        if (voucher.getStock() < 1) {
+            return Result.fail("秒杀券已抢空");
+        }
+        // 5、秒杀券合法，则秒杀券抢购成功，秒杀券库存数量减一
+        boolean flag = seckillVoucherService.update(new LambdaUpdateWrapper<SeckillVoucher>()
+                .eq(SeckillVoucher::getVoucherId, voucherId)
+                .setSql("stock = stock -1"));
+        if (!flag){
+            throw new RuntimeException("秒杀券扣减失败");
+        }
+        // 6、秒杀成功，创建对应的订单，并保存到数据库
+        VoucherOrder voucherOrder = new VoucherOrder();
+        long orderId = redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        voucherOrder.setUserId(UserHolder.getUser().getId());
+        voucherOrder.setVoucherId(voucherId);
+        flag = this.save(voucherOrder);
+        if (!flag){
+            throw new RuntimeException("创建秒杀券订单失败");
+        }
+        // 返回订单id
+        return Result.ok(orderId);
+    }
 
 }
