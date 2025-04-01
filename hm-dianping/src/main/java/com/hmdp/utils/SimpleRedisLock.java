a4650fba957b0547f69e5641c0a8634bec7a95d2
xiaozayula <981446636@qq.com>
2025-04-02 00:00:37 +0800

    分布式锁优化,Lua脚本

--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -45,16 +48,39 @@ public class SimpleRedisLock implements  ILock{
     }
 
     /**
+     * 加载Lua脚本
+     */
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
+    /**
      * 释放锁
      */
     @Override
     public void unlock() {
-        String threadId = ID_PREFIX+Thread.currentThread().getId() ;
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if (threadId.equals(id)) {
-            // 一致，说明当前的锁就是当前线程的锁，可以直接释放
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
-
+        // 执行lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+        );
     }
+//    /**
+//     * 释放锁
+//     */
+//    @Override
+//    public void unlock() {
+//        String threadId = ID_PREFIX+Thread.currentThread().getId() ;
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        if (threadId.equals(id)) {
+//            // 一致，说明当前的锁就是当前线程的锁，可以直接释放
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+//
+//    }
 }
