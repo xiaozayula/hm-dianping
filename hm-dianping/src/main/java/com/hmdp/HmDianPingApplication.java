23176b291f7a7359e1df4e55ca4c7a397640203e
xiaozayula <981446636@qq.com>
2025-04-01 20:07:53 +0800

    添加aspectjweaver依赖

--- hm-dianping/src/main/java/com/hmdp/HmDianPingApplication.java
+++ hm-dianping/src/main/java/com/hmdp/HmDianPingApplication.java
@@ -3,8 +3,9 @@ package com.hmdp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+@EnableAspectJAutoProxy(exposeProxy = true)
 @EnableTransactionManagement
 @MapperScan("com.hmdp.mapper")
 @SpringBootApplication
